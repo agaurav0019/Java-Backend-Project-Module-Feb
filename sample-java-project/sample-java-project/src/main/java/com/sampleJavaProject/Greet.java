0f06d94bd3f82ba2ca54b5fd4ee738b68f00aed1
Abhishek Gaurav <gaurav.abhishek99@gmail.com>
2026-05-02 21:17:56 +0530

    running the jar file by specifying main class in the build file

---
+++ sample-java-project/sample-java-project/src/main/java/com/sampleJavaProject/Greet.java
@@ -0,0 +1,23 @@
+package com.sampleJavaProject;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Greet {
+    public static final String GREET_MORN = "Good Morning";
+    public static final String GREET_AFT = "Good Afternoon";
+    public static final String GREET_EVEN = "Good Evening";
+
+    public static String fetchGreeting() {
+        int hotd = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+        if (hotd < 12) {
+            return GREET_MORN;
+        }
+        else if (hotd < 18) {
+            return GREET_AFT;
+        } else {
+            return GREET_EVEN;
+        }
+    }
+}
