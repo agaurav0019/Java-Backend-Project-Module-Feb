5cb274dd891a9aa3975086f18eb06d25b5cbfb7d
Abhishek Gaurav <gaurav.abhishek99@gmail.com>
2026-05-05 20:29:27 +0530

    Created new project for task Manager application

--- toDoList/src/main/java/com/springtut/todolist/controllers/TaskController.java
+++ toDoList/src/main/java/com/springtut/todolist/controllers/TaskController.java
@@ -24,5 +24,16 @@ public class TaskController {
         taskList.add(taskToAdd);
         return ResponseEntity.status(201).body(taskToAdd);
     }
-
+    /**
+     * ASSIGNEMENT
+     * 1. GET -> /tasks/3
+     *              get task no 3
+     *              Send 404 error to client if task no, 3 doesn't exist
+     * 2. PATCH -> /tasks/3
+     *              Update due date, or task staus for task no. 3
+     *              Send 404 error to client if task 3 doesn't exist
+     * 3. DELETE -> /tasks/3
+     *              Delete task no. 3 (Response with correct http code)
+     *              if task no. 5 doesn't exist send 404 error
+     */
 }
